9765f477f0bee09198b1f3c353889418ff6cce5f
AakashShukla18 <aakash.s@virtualizetechnologies.com>
2026-07-03 16:29:08 +0530

    added portrait view for pc

--- src/Experience/Utils/Sizes.js
+++ src/Experience/Utils/Sizes.js
@@ -7,15 +7,15 @@ export default class Sizes extends EventEmitter
         super()
 
         // Setup
-        this.width = window.innerWidth
-        this.height = window.innerHeight
+        this.width = document.body.clientWidth || window.innerWidth
+        this.height = document.body.clientHeight || window.innerHeight
         this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
         // Resize event
         window.addEventListener('resize', () =>
         {
-            this.width = window.innerWidth
-            this.height = window.innerHeight
+            this.width = document.body.clientWidth || window.innerWidth
+            this.height = document.body.clientHeight || window.innerHeight
             this.pixelRatio = Math.min(window.devicePixelRatio, 2)
 
             this.trigger('resize')
