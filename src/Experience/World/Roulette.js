1482385217662151317c1857b153d0a884508088
AakashShukla18 <aakash.s@virtualizetechnologies.com>
2026-08-05 11:22:10 +0530

    updated the text position and scale of roulette cube counter UI

--- src/Experience/World/Roulette.js
+++ src/Experience/World/Roulette.js
@@ -208,9 +208,9 @@ export default class Roulette {
         }
         
         // Apply explicitly requested fixed values
-        this.textParams.y = 0.03
+        this.textParams.y = 0.039
         this.textParams.z = 1.3
-        this.textParams.scale = 0.43
+        this.textParams.scale = 0.38
 
         this.textMesh.position.set(this.textParams.x, this.textParams.y, this.textParams.z)
         this.textMesh.rotation.set(this.textParams.rotX, this.textParams.rotY, this.textParams.rotZ)
